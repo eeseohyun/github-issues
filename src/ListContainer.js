a20be6368e983d7bae743b58fc7ba87abb9efea1
eeseohyun <jinlijinyoung@gmail.com>
2023-04-04 18:59:35 +0900

    Pagination 버튼 적용

--- src/ListContainer.js
+++ src/ListContainer.js
@@ -5,55 +5,63 @@ import ListItem from "./components/ListItem";
 import ListItemLayout from "./components/ListItemLayout";
 import cx from "clsx";
 import Modal from "./components/Modal";
+import Pagination from "./components/Pagination";
 
 export default function ListContainer() {
 	const [inputValue, setInputValue] = useState("is:issue is:open");
+	const [list, setList] = useState([]);
 	const [checkedList, setCheckedList] = useState([]);
-	const [list, setList] = useState("받아온 데이터");
+	const [page, setPage] = useState(1);
 
 	// const data = getDate();
 	// const openedData = data.filter((d)=>d.state === 'open')
 	// const closedData = data.filter((d)=>d.state === 'closed')
-
+	//const MAX_PAGE = getData().totalCount;
+	//totalCount 100 / 30  3.3333 4page까지는 나옴
 	return (
-		<div className={styles.listContainer}>
-			<div className={styles.topSection}>
-				<input
-					className={styles.input}
-					value={inputValue}
-					onChange={(e) => setInputValue(e.target.value)}
-				/>
-				<Button
-					style={{
-						fontSize: "14px",
-						backgroundColor: "green",
-						color: "white",
-					}}
-				>
-					New Issue
-				</Button>
-			</div>
-			<OpenClosedFilters />
-			<ListItemLayout className={styles.listFilter}>
-				<ListFilter
-				// onChangeFilter={
-				// 	(filteredData) => {}
-				// 	//필터링된 요소에 맞게 데이터 불러오기
-				// 	//const data = getData('필터링된 정보')
-				// 	//setList(data)
-				// }
-				/>
-			</ListItemLayout>
-			<div className={styles.container}>
-				{list.map((listItem, idx) => (
-					<ListItem
-						key={idx}
-						checked={checkedList.filter((item) => item.id === "0")[0]}
-						badges={[{ color: "red", title: "bug" }]}
+		<>
+			<div className={styles.listContainer}>
+				<div className={styles.topSection}>
+					<input
+						className={styles.input}
+						value={inputValue}
+						onChange={(e) => setInputValue(e.target.value)}
 					/>
-				))}
+					<Button
+						style={{
+							fontSize: "14px",
+							backgroundColor: "green",
+							color: "white",
+						}}
+					>
+						New Issue
+					</Button>
+				</div>
+				<OpenClosedFilters />
+				<ListItemLayout className={styles.listFilter}>
+					<ListFilter
+					// onChangeFilter={
+					// 	(filteredData) => {}
+					// 	//필터링된 요소에 맞게 데이터 불러오기
+					// 	//const data = getData('필터링된 정보')
+					// 	//setList(data)
+					// }
+					/>
+				</ListItemLayout>
+				<div className={styles.container}>
+					{list.map((listItem, idx) => (
+						<ListItem key={idx} badges={[{ color: "red", title: "bug" }]} />
+					))}
+				</div>
 			</div>
-		</div>
+			<div className={styles.paginationContainer}>
+				<Pagination
+					maxPage={10}
+					currentPage={page}
+					onClickPageButton={(number) => setPage(number)}
+				/>
+			</div>
+		</>
 	);
 }
 
